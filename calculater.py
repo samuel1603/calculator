ee3e71d913794b1daaaa90123540739be7ebd88e
Samuel M <75807646+samuel1603@users.noreply.github.com>
2021-02-27 07:23:46 +0530

    Add files via upload

---
+++ calculater.py
@@ -0,0 +1,23 @@
+a=int(input("enter the num 1:"))
+b=int(input("enter the num 2:"))
+
+def add(a,b):
+    return(a+b)
+def sub(a,b):
+    return(a-b)
+def multi(a,b):
+    return(a*b)
+def div (a,b):
+    return(a/b)
+def exp (a,b):
+    return(a**b)
+def per (a,b):
+    return(a%b)
+    
+print(add(a,b))
+print(sub(a,b))
+print(multi(a,b))
+print(div(a,b))
+print(exp(a,b))
+print(per(a,b))
+
